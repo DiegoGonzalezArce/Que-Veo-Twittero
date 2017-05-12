79f91bc4dc56d47702be9da780ced8019b8ebdd9
Maitesz <masz61995@gmail.com>
2017-05-11 21:56:29 -0300

    Archivos de front-end

---
+++ TwiteroFRONT/app.js
@@ -0,0 +1,14 @@
+
+
+(function (){ 
+            var app = angular.module('myApp', []);
+            app.controller('customerCtrl', function(){
+            this.product = programa;
+
+            });
+            var programa = {
+                Nombre: 'masterchef',
+                Canal: 'trece',
+                description:'lalala',
+            }
+})();
